0110dace23e3cab35b29d2b402c1c833e8ec1ba6
unknown <hoangdung300798@gmail.com>
2023-07-11 22:13:21 +0700

    commit 1

---
+++ src/components/ChatInput.jsx
@@ -0,0 +1,146 @@
+import React, { useState } from 'react';
+import { styled } from 'styled-components';
+import EmojiPicker  from 'emoji-picker-react'
+import { IoMdSend } from 'react-icons/io';
+import { BsEmojiSmileFill } from 'react-icons/bs'
+
+const ChatInput = (props) => {
+    const { handleSendMsg } = props;
+    const [showEmojiPicker, setShowEmojiPicker] = useState(false)
+    const [msg, setMsg] = useState('')
+
+    const handleEmojiPickerHideShow = () => {
+        setShowEmojiPicker(!showEmojiPicker)
+    }
+
+    const handleEmojiClick = (emoji, event) => {
+        let message = msg;
+        message += emoji.emoji
+        setMsg(message)
+    }
+
+    const sendChat = (event) => {
+        event.preventDefault();
+        if (msg.length > 0) {
+            handleSendMsg(msg);
+            setMsg('')
+        }
+    }
+
+    return (
+        <>
+            <Container>
+                <div className='button-container'>
+                    <div className='emoji'>
+                        <BsEmojiSmileFill onClick={handleEmojiPickerHideShow} />
+                        { showEmojiPicker && <EmojiPicker onEmojiClick={handleEmojiClick} />}
+                    </div>
+                </div>
+                <form className='input-container' onSubmit={(e) => sendChat(e)}>
+                    <input type='text' placeholder='Type your message here' value={msg} onChange={(e) => setMsg(e.target.value)} />
+                    <button className='submit'>
+                        <IoMdSend />
+                    </button>
+                </form>
+            </Container>
+        </>
+    );
+};
+
+const Container = styled.div`
+    display: grid;
+    grid-template-columns: 5% 95%;
+    background-color: #080420;
+    align-items: center;
+    padding: 0 2rem;
+    padding-bottom: 0.3rem;
+    position: relative;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+        padding: 0 1rem;
+        gap: 1rem;
+    }
+    .button-container {
+        display: flex;
+        align-items: center;
+        color: white;
+        gap: 1rem;
+        .emoji {
+            svg {
+                font-size: 1.5rem;
+                color: #ffff00c8;
+                cursor: pointer;
+            }
+            .EmojiPickerReact {
+                position: absolute;
+                bottom: 50px;
+                background-color: #080420;
+                box-shadow: 0 5px 10px #9a86f3;
+                border-color: #9a86f3;
+                .epr-body::-webkit-scrollbar {
+                    background-color: #080420;
+                    width: 5px;
+                    &-thumb {
+                        background-color: #9186f3;
+                    }
+                }
+                .epr-category-nav {
+                    button {
+                        /* filter: contrast(0); */
+                    }
+                }
+                .epr-search-container {
+                    background: transparent;
+                    border-color: #9186f3;
+                }
+                .epr-emoji-category-label {
+                    background-color: #080420;
+                }
+            }
+        }
+    }
+    .input-container {
+        width: 100%;
+        border-radius: 2rem;
+        display: flex;
+        align-items: center;
+        gap: 2rem;
+        background-color: #ffffff34;
+        input {
+            width: 90%;
+            height: 60%;
+            background-color: transparent;
+            color: white;
+            border: none;
+            font-size: 1.2rem;
+            padding-left: 1rem;
+            &::selection {
+                background-color: #9186f3;
+            }
+            &:focus {
+                outline: none;
+            }
+        }
+        button {
+            padding: 0.3rem 2rem;
+            border-radius: 2rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #9a86f3;
+            border: none;
+            cursor: pointer;
+            @media screen and (min-width: 720px) and (max-width: 1080px) {
+                padding: 0.3rem 1rem;
+                svg {
+                    font-size: 1rem;
+                }
+            }
+            svg {
+                font-size: 2rem;
+                color: white;
+            }
+        }
+    }
+`
+
+export default ChatInput;
---
+++ src/utils/ApiRoutes.js
@@ -0,0 +1,8 @@
+export const host = "http://localhost:5000"
+
+export const registerRouter = `${host}/api/auth/register`
+export const loginRouter = `${host}/api/auth/login`
+export const setAvatarRouter = `${host}/api/auth/setAvatar`
+export const allUserRouter =  `${host}/api/auth/allusers`
+export const sendMessageRouter =  `${host}/api/messages/addmsg`
+export const getAllMessageRouter =  `${host}/api/messages/getmsg`
